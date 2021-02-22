d78e9a4efda09d5ec35038f3cc60d3b571316d3c
heidysnj <heidysnunez15@gmail.com>
2021-02-22 21:41:16 +0000

    complete

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -10,12 +10,20 @@ export function Home() {
 	const [time4, setTime4] = useState(0);
 	const [time5, setTime5] = useState(0);
 	const [time6, setTime6] = useState(0);
+	const [pause, setPause] = useState(true);
+	let interval = null;
 
 	useEffect(() => {
-		setInterval(() => {
-			setTime(time => time + 1);
-		}, 1000);
-	}, []);
+		if (pause) {
+			interval = setInterval(() => {
+				setTime(time => time + 1);
+			}, 1000);
+		} else if (!pause && time !== 0) {
+			clearInterval(interval);
+		}
+		return () => clearInterval(interval);
+	}, [pause, time]);
+
 	if (time === 10) {
 		setTime(0);
 		setTime2(time2 + 1);
@@ -45,6 +53,23 @@ export function Home() {
 		setTime4(0);
 		setTime5(0);
 		setTime6(time6 + 1);
+	}
+	function start() {
+		setPause(!pause);
+	}
+
+	function stop() {
+		setPause(!pause);
+	}
+
+	function reset() {
+		setTime(0);
+		setTime2(0);
+		setTime3(0);
+		setTime4(0);
+		setTime5(0);
+		setTime6(0);
+		setPause(pause);
 	}
 
 	return (
@@ -85,10 +110,25 @@ export function Home() {
 					</h1>
 				</div>
 			</div>
-			<div>
-				<StopButton />
-				<ResumeButton />
-				<ResetButton />
+			<div className="mt-5">
+				<button
+					type="button"
+					className="btn btn-outline-secondary"
+					onClick={() => stop()}>
+					Stop
+				</button>
+				<button
+					type="button"
+					className="btn btn-outline-secondary"
+					onClick={() => start()}>
+					Resume
+				</button>
+				<button
+					type="button"
+					className="btn btn-outline-secondary"
+					onClick={() => reset()}>
+					Reset
+				</button>
 			</div>
 		</div>
 	);
